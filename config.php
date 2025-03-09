86df83dc8c4549832972957257132ad8b56a03be
P.S.Adhithya <p.s.adhithyaa@gmail.com>
2025-03-09 15:24:40 +0530

    Updated config.php and added cache folder

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = ""; // Update if you set a password
+$db_pass = "newpassword"; // Update if you set a password
 $db_name = "iwatch";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
